9ef303209b7aae1b48abb34dc8d10002a56d221f
Minh12s <minhtnth2209037@fpt.edu.vn>
2024-04-24 00:39:05 +0700

    template user

---
+++ Controllers/PageController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineCollegeManagement.Controllers
+{
+    public class PageController : Controller
+    {
+        public async Task<IActionResult> Home()
+        {
+            return View();
+        }
+        public async Task<IActionResult> About()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Courses()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Teacher()
+        {
+            return View();
+        }
+        public async Task<IActionResult> TeacherDetails()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Blog()
+        {
+            return View();
+        }
+        public async Task<IActionResult> BlogDetails()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Events()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contact()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Page}/{action=Home}/{id?}");
 
 app.Run();
