266b782997d160f4e969fae4946298d81720f187
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2024-04-26 16:29:06 +0700

    update template Admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineCollegeManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View("DashboardAdmin/Dashboard");
+        }
+        public IActionResult Student()
+        {
+            return View("StudentManagement/Student");
+        }
+      
+        public IActionResult EditStudent()
+        {
+            return View("StudentManagement/EditStudent");
+        }
+
+        public IActionResult Admissions()
+        {
+            return View("AdmissionsManagement/Admissions");
+        }
+
+        public IActionResult AdmissionsDetail()
+        {
+            return View("AdmissionsManagement/AdmissionsDetail");
+        }
+
+        public IActionResult Course()
+        {
+            return View("CourseManagement/Course");
+        }
+
+
+        public IActionResult AddCourse()
+        {
+            return View("CourseManagement/AddCourse");
+        }
+
+        public IActionResult EditCourse()
+        {
+            return View("CourseManagement/EditCourse");
+        }
+
+
+        public IActionResult Lecturer()
+        {
+            return View("LecturerManagement/Lecturer");
+        }
+
+        public IActionResult AddLecturer()
+        {
+            return View("LecturerManagement/AddLecturer");
+        }
+
+        public IActionResult EditLecturer()
+        {
+            return View("LecturerManagement/EditLecturer");
+        }
+
+    }
+}
