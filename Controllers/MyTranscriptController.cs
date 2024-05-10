361b0d91ef436b787de709350748a0f861bd54a5
Minh12s <minhtnth2209037@fpt.edu.vn>
2024-05-10 13:54:22 +0700

    update  giao diện my transcript

---
+++ Controllers/MyTranscriptController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineCollegeManagement.Data;
+using OnlineCollegeManagement.Models;
+using BCrypt.Net;
+using System.Text;
+using System.Net.Mail;
+using System.Drawing.Printing;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata;
+using System.Net;
+using System.IO;
+using OnlineCollegeManagement.Models.Authentication;
+
+
+namespace OnlineCollegeManagement.Controllers
+{
+    [Authentication]
+
+    public class MyTranscriptController : Controller
+    {
+        private readonly CollegeManagementContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public MyTranscriptController(CollegeManagementContext context, IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _context = context;
+            _configuration = configuration;
+            _env = env;
+        }
+        public async Task<IActionResult> MyTranscript()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        public async Task<IActionResult> MyTimetable()
+        {
+            return View();
+        }
+    }
+}
