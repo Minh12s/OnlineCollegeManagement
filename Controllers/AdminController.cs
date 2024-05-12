e33100833cda356c96dcd1dd75c2799fa4ca5825
dungdang <dungprohn1409@gmail.com>
2024-05-12 16:27:47 +0700

    update dashboard

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -33,10 +33,59 @@ namespace OnlineCollegeManagement.Controllers
             _env = env;
         }
         [Authentication]
-        public IActionResult Dashboard()
+        public IActionResult Dashboard(int page = 1)
         {
+            // Courses
+            var recentCourses = _context.Courses
+               .OrderByDescending(c => c.CoursesId)
+               .Take(10)
+               .ToList();
+            // Classes
+            var recentClasses = _context.Classes
+               .OrderByDescending(c => c.StartDate)
+               .Take(10)
+               .ToList();
+            // pending Registrations
+            int pageSize = 5;
+
+            var pendingRegistrations = _context.Registrations
+                .Include(r => r.StudentInformation)
+                .Where(r => r.RegistrationStatus == "Pending")
+                .OrderByDescending(r => r.RegistrationDate)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            int totalClasses = _context.Classes.Count();
+            int totalCourses = _context.Courses.Count();
+            int totalStudents = _context.OfficialStudents.Count();
+            int totalTeachers = _context.Teachers.Count();
+            int totalEvents = _context.Events.Count();
+            int totalMajors = _context.Majors.Count();
+
+            int totalPendingRegistrations = _context.Registrations
+                .Count(r => r.RegistrationStatus == "Pending");
+            
+           
+            ViewBag.RecentCourses = recentCourses;
+            ViewBag.RecentClasses = recentClasses;
+            ViewBag.PendingRegistrations = pendingRegistrations;
+            ViewBag.TotalClasses = totalClasses;
+            ViewBag.TotalCourses = totalCourses;
+            ViewBag.TotalStudents = totalStudents;
+            ViewBag.TotalTeachers = totalTeachers;
+            ViewBag.TotalEvents = totalEvents;
+            ViewBag.TotalMajors = totalMajors;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling(totalPendingRegistrations / (double)pageSize);
+
             return View();
         }
+
+
+
+
+
 
         [HttpGet]
         public IActionResult Login()
